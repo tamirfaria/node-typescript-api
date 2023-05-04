628d4db1ba12953e0cb233c5d889243854fba67f
Lucas Tamir Faria <lucastamir@gmail.com>
2023-05-04 09:28:45 -0300

    :white_check_mark: adding unit test - C02P01-1
    C02P01-1

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,18 @@
+import axios from "axios"
+import { StormGlass } from "../stormGlass"
+
+jest.mock('axios')
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+
+    const lat = -33.792726
+    const lng = 151.289824
+
+    axios.get = jest.fn().mockResolvedValue({});
+
+    const stormGlass = new StormGlass(axios)
+    const response = await stormGlass.fetchPoints(lat, lng)
+    expect(response).toEqual({})
+  })
+})
---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,15 @@
+import { AxiosStatic } from "axios";
+
+export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassAPISource = 'noaa';
+
+  constructor(protected request: AxiosStatic) { }
+
+  public async fetchPoints(lat: number, lng: number): Promise<object> {
+    const url = `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
+    
+    return this.request.get(url)
+  }
+}
