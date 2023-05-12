e34544f02effe2ddf3825c3aed8c7648f94edef2
Lucas Tamir Faria <lucastamir@gmail.com>
2023-05-12 09:51:40 -0300

    :white_check_mark: Updating tests with new errors instances - C02P04
    C02P04

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,18 +1,19 @@
+import * as HTTPUtil from '@src/util/request'
 import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
-import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
-import axios from "axios"
+import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json'
 import { StormGlass } from "../stormGlass"
 
-jest.mock('axios')
+jest.mock('@src/util/request')
 
 describe('StormGlass client', () => {
-  const mockedAxios = axios as jest.Mocked<typeof axios>
   const lat = -33.792726
   const lng = 151.289824
-  
+  const MockedRequestClass = HTTPUtil.Request as jest.Mocked<typeof HTTPUtil.Request>
+  const mockedRequest = new HTTPUtil.Request() as jest.Mocked<HTTPUtil.Request>
+
   it('should return the normalized forecast from the StormGlass service', async () => {
-    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
-    const stormGlass = new StormGlass(mockedAxios)
+    mockedRequest.get.mockResolvedValue({ data: stormglassWeatherPointFixture } as HTTPUtil.Response)
+    const stormGlass = new StormGlass(mockedRequest)
     const response = await stormGlass.fetchPoints(lat, lng)
     expect(response).toEqual(stormGlassNormalized3HoursFixture)
   })
@@ -21,38 +22,43 @@ describe('StormGlass client', () => {
     const incompleteResponse = {
       hours: [
         {
-          windDirection: {
-            noaa: 300,
-          },
+          windDirection: { noaa: 300 },
           time: '2020-04-26T00:00:00:00+00:00'
         }
       ]
     }
-    mockedAxios.get.mockResolvedValue({ data: incompleteResponse })
-    const stormGlass = new StormGlass(mockedAxios)
+    mockedRequest.get.mockResolvedValue({ data: incompleteResponse } as HTTPUtil.Response)
+    const stormGlass = new StormGlass(mockedRequest)
     const response = await stormGlass.fetchPoints(lat, lng)
     expect(response).toEqual([])
   })
 
   it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
-    mockedAxios.get.mockRejectedValue({message: 'Network Error'})
-    const stormGlass = new StormGlass(mockedAxios)
+    mockedRequest.get.mockRejectedValue({ message: 'Network Error' })
+    const stormGlass = new StormGlass(mockedRequest)
     const response = stormGlass.fetchPoints(lat, lng)
-    await expect(response).rejects.toThrow('Unexpected error when trying to communicate to StormGlass: Network Error')
+    await expect(response).rejects.toThrow(`Unexpected error when trying to communicate to StormGlass: {"message":"Network Error"}`)
   })
 
   it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
-    mockedAxios.get.mockRejectedValue({
-      response: {
-        status: 429,
-        data: {
-          errors: ['Rate Limite reached']
-        }
+    class FakeAxiosError extends Error {
+      constructor(public response: object) {
+        super()
       }
-    })
-    const stormGlass = new StormGlass(mockedAxios)
+    }
+
+    const rateLimitMessage = {
+      status: 429,
+      data: { errors: ['Rate Limite reached'] }
+    }
+
+    mockedRequest.get.mockRejectedValue(new FakeAxiosError(rateLimitMessage))
+    MockedRequestClass.isRequestError.mockReturnValue(true)
+    MockedRequestClass.extractErrorData.mockReturnValue(rateLimitMessage)
+    const stormGlass = new StormGlass(mockedRequest)
+    
     await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
-      'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limite reached"]} Code: 429'
-      )
+      'Unexpected error returned by the StormGlass service: Erro: {"errors":["Rate Limite reached"]} Code: 429'
+    )
   })
 })
